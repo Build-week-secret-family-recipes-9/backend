91783cce110e52fd0ab31742a81f5f415942ecd2
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-27 07:31:55 -0800

    Touchup

--- userapp/node-auth2-guided/index.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-
-const server = require("./api/server.js");
-
-const port = process.env.NOPORT || 8045;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
