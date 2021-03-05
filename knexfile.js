00ed51ad693080d91f5422d91f8f8e7e608667e9
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-03-05 08:01:03 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,21 @@
-const pgConnection = process.env.DATABASE_URL;
 module.exports = {
+
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
+    useNullAsDefault:true,
     connection: {
-      filename: "/database/auth.db3",
+      filename: './database/auth.db3'
     },
     migrations: {
-      directory: "./database/migrations",
+      directory:'./database/migrations'
     },
-    seeds: {
-      directory: "./database/seeds",
+    seeds:{
+      directory: './database/seeds'
     },
+    pool: { 
+      afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) 
+    }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
 
-  production: {
-    client: "pg",
-    connection: {
-      connectionString: pgConnection,
-      ssl: { rejectUnauthorized: false },
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
 };
