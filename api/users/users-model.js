b3b5bb1994dcd29b99d216d2cb77fdbd9a31968b
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-03-05 18:11:41 -0800

    Update users-model.js

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,31 +1,26 @@
 const db = require("../../database/connection.js");
 
 module.exports = {
-  add,
-  find,
-  findBy,
-  findById,
-};
+    add, 
+    find, 
+    findBy,
+    findById
+  }
+  
+  function find() {
+    return db("users").select("id", "username", "password").orderBy("id");
+  }
+  
+  function findBy(filter) {
+    return db("users").where(filter).orderBy("id");
+  }
+  
+  async function add(user) {
+    const [id] = await db("users").insert(user, "id");
+    return findById(id);
+  }
+  
+  function findById(id) {
+    return db("users").where({ id }).first();
+  }
 
-function find() {
-  return db("users as u")
-    .select("u.id", "u.username");
-}
-
-function findBy(filter) {
-  return db("users as u")
-    .select("u.id", "u.username","u.password")
-    .where(filter);
-}
-
-async function add(user) {
-  const [id] = await db("users").insert(user, "id");
-  return findById(id);
-}
-
-function findById(id) {
-  return db("users as u")
-    .select("u.id", "u.username")
-    .where("u.id", id)
-    .first();
-}
